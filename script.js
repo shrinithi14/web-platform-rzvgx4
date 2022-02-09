c704f0cd4a37752e5cd351dacbec98a9b6c5c105
shrinithi14 <shri94nithi@gmail.com>
2022-02-09 22:17:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function goHome(){
+  window.open('index.html','_self');
+}
+function openMe(){
+  window.open('popup.html','_self');
+}
+function closeMe(){
+  window.open('','_self','')
+  window.close();
+  //         var result = confirm(
+  //   'The webpage you are viewing is trying to close the window. Do you want to close this window'
+  // );
+  // if (result) {
+  //     }
+    }
